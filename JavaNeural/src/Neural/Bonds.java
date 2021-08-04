27e7a72ec7e236d352b041ef0f87ed9c2a1825e6
nBerkSahin <nazmiberksahin>
2021-08-04 17:53:41 +0300

    first commit

---
+++ JavaNeural/src/Neural/Bonds.java
@@ -0,0 +1,13 @@
+package Neural;
+
+public class Bonds {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+        System.out.println("Construction complete");
+        System.out.println("Try it");
+    }
+
+
+}
